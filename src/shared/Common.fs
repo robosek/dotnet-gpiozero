9b4a442136f3a5fd6efd61b35105268fdcdf3011
Robert Sęk <robosek@tlen.pl>
2019-04-27 14:22:18 +0200

    Convert to module again

--- src/shared/Common.fs
+++ src/shared/Common.fs
@@ -1,49 +1,54 @@
 namespace DotnetGpiozero.Shared
 open System.Device.Gpio
 
-type Common() =
+[<AutoOpen>]
+module Common =
 
     [<Literal>]
     let SoftPwmFrequencyHz = 50.0
 
-    member __.Write pinValue (OpenedPin (pin, gpioController)) =
+    let write pinValue (OpenedPin (pin, gpioController)) =
         gpioController.Write(pin, pinValue )
     
-    member __.WritePwm pinValue (OpenedPwmPin (pwmPin, pwmController)) =
-        let (pin, channel) = pwmPin
-        pwmController.ChangeDutyCycle(pin, channel, pinValue * 100.0)
+    let writePwm pinValue (OpenedPwmPin (pwmPin, pwmController)) =
+        pwmController.ChangeDutyCycle(pwmPin, 0, pinValue * 100.0)
 
-    member __.TryOpenPin (NotCheckedPin (pin, gpioController)) : Result<OpenedPin, string> =
+    let tryOpenPin (NotCheckedPin (pin, gpioController)) : Result<OpenedPin, string> =
         try
             match gpioController.IsPinOpen pin with
             | true -> Ok(OpenedPin(pin, gpioController))
             | false -> gpioController.OpenPin(pin, PinMode.Output)
                        let openedPin = OpenedPin(pin, gpioController)
-                       __.Write PinValue.Low openedPin
+                       write PinValue.Low openedPin
                        Ok(openedPin)
         with
         | ex -> Error ex.Message
 
-    member __.TryOpenPwmPin (NotCheckedPwmPin (pinPwm, pwmController)) : Result<OpenedPwmPin, string> =
+    let tryOpenPwmPin (NotCheckedPwmPin (pinPwm, pwmController)) : Result<OpenedPwmPin, string> =
         try
-           let (pin, channel) = pinPwm
-           pwmController.OpenChannel(pin, channel)
-           pwmController.StartWriting(pin, channel, SoftPwmFrequencyHz, 100.0)
+           pwmController.OpenChannel(pinPwm, 0)
+           pwmController.StartWriting(pinPwm, 0, SoftPwmFrequencyHz, 100.0)
            Ok(OpenedPwmPin(pinPwm, pwmController))
         with
         | ex -> Error ex.Message
 
-    member __.TryClosePin (OpenedPin (pin, gpioController)) : Result<Unit,string> =
+    let tryClosePin (OpenedPin (pin, gpioController)) : Result<Unit,string> =
         try
             gpioController.ClosePin(pin)
             Ok(gpioController.Dispose())
         with
         | ex -> Error ex.Message
 
-    member __.TryClosePwmPin (OpenedPwmPin (pin, pwmController)) : Result<Unit,string> =
+    let tryClosePwmPin (OpenedPwmPin (pin, pwmController)) : Result<Unit,string> =
         try
-            let (pin, channel) = pin
-            pwmController.CloseChannel(pin, channel)
+            pwmController.CloseChannel(pin, 0)
             Ok(pwmController.Dispose())
         with
         | ex -> Error ex.Message
+    
+    let createTimerAndObservable timerInterval =
+        let timer = new System.Timers.Timer(float timerInterval)
+        timer.AutoReset <- true
+        let observable = timer.Elapsed  
+        let task = async {timer.Start()}
+        {Task = task; Observable =  observable; StopTask = timer.Stop}
