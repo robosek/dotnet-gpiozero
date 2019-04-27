ea90327770d4b8f99b7ed883f3024606eec31994
Robert Sęk <robosek@tlen.pl>
2019-04-27 14:22:42 +0200

    Use common module

--- src/output-devices/robot/Robot.fs
+++ src/output-devices/robot/Robot.fs
@@ -2,52 +2,59 @@ namespace DotnetGpiozero.Robot
 open DotnetGpiozero.Shared
 open System.Device.Gpio
 
-type RobotController(common: Common) =
+type RobotController() =
 
     let motorForward speed (Motor motor) =
         let (pin1, pinPwm) = motor
 
-        pin1 |> common.Write PinValue.High
-        pinPwm |> common.WritePwm (1.0 - speed)
+        pin1 |> write PinValue.High
+        pinPwm |> writePwm (1.0 - speed)
 
-    let motorBack speed (Motor motor) =
+    let motorBackward speed (Motor motor) =
         let (pin1, pinPwm) = motor
 
-        pin1 |> common.Write PinValue.Low
-        pinPwm |> common.WritePwm speed
+        pin1 |> write PinValue.Low
+        pinPwm |> writePwm speed
     
     let motorStop (Motor motor) =
         let (pin1, pinPwm) = motor
 
-        pin1 |> common.Write PinValue.Low
-        pinPwm |> common.WritePwm 0.0
+        pin1 |> write PinValue.Low
+        pinPwm |> writePwm 0.0
 
     member __.TryOpenMotorPins (pins : (NotCheckedPin *  NotCheckedPwmPin)) = 
         let (pin1, pinPwm) = pins
-        let pinLeft = common.TryOpenPin pin1
-        let pinPwm = common.TryOpenPwmPin pinPwm
+        let pinLeft = tryOpenPin pin1
+        let pinPwm = tryOpenPwmPin pinPwm
         
         match pinLeft,pinPwm with
         | Ok pinLeft,Ok pinPwm-> Ok(Motor(pinLeft,pinPwm))
         | _, Error e -> Error(e)
         | Error e,_ -> Error(e)
-     
 
     member __.Forward speed (robot: Robot) = 
         robot.LeftMotor  |> motorForward speed 
         robot.RightMotor |> motorForward speed  
 
-    member __.Left speed (robot: Robot) = 
-        robot.LeftMotor |> motorForward 0.2
+    member __.LeftSoft speed (robot: Robot) = 
+        robot.LeftMotor |> motorForward 0.15
         robot.RightMotor |> motorForward speed
 
-    member __.Right speed (robot: Robot)  = 
+    member __.LeftInPlace speed (robot: Robot) = 
+        robot.LeftMotor |> motorBackward speed
+        robot.RightMotor |> motorForward speed
+
+    member __.RightSoft speed (robot: Robot)  = 
         robot.LeftMotor |> motorForward speed 
-        robot.RightMotor |> motorForward 0.2 
+        robot.RightMotor |> motorForward 0.15
+
+    member __.RightInPlace speed (robot: Robot)  = 
+        robot.LeftMotor |> motorForward speed 
+        robot.RightMotor |> motorBackward speed
     
     member __.Backward speed (robot: Robot) =
-        robot.LeftMotor |> motorBack speed  
-        robot.RightMotor |> motorBack speed
+        robot.LeftMotor |> motorBackward speed  
+        robot.RightMotor |> motorBackward speed
     
     member __.Stop (robot: Robot) = 
         robot.LeftMotor |> motorStop 
