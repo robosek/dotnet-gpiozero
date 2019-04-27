bc77f8ce47bff27bdb41bdc5d5b116b187c34448
Robert Sęk <robosek@tlen.pl>
2019-04-27 14:22:04 +0200

    Make pin types private with constructor

--- src/shared/Types.fs
+++ src/shared/Types.fs
@@ -1,11 +1,30 @@
 namespace DotnetGpiozero.Shared
 open System.Device.Gpio
 open System.Device.Pwm
+open System.Device.Pwm.Drivers
 
 type Pin = int
-type Channel = int
-type PwmPin = Pin * Channel
-type NotCheckedPin = NotCheckedPin of Pin * GpioController
-type NotCheckedPwmPin = NotCheckedPwmPin of PwmPin * PwmController
-type OpenedPin = OpenedPin of Pin * GpioController
-type OpenedPwmPin = OpenedPwmPin of PwmPin * PwmController
+type PwmPin = int
+
+type NotCheckedPin = private NotCheckedPin of Pin * GpioController
+                     with static member Create pin = 
+                            NotCheckedPin(pin, new GpioController())
+
+type NotCheckedPwmPin = private NotCheckedPwmPin of PwmPin * PwmController
+                        with static member Create pin = 
+                              NotCheckedPwmPin(pin, new PwmController(new SoftPwm(true))) 
+                                
+type OpenedPin = private OpenedPin of Pin * GpioController
+                  with static member Create pin gpioController = 
+                               OpenedPin(pin, gpioController)
+
+type OpenedPwmPin = private OpenedPwmPin of PwmPin * PwmController
+                    with static member Create pin pwmController = 
+                                 OpenedPwmPin(pin, pwmController)
+
+type EventStream = 
+        {
+            Task: Async<unit>; 
+            Observable: IEvent<System.Timers.ElapsedEventHandler,System.Timers.ElapsedEventArgs>
+            StopTask: Unit -> Unit
+        }
