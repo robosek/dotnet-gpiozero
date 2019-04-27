790d80e43f4ca115587b5af0fef95cb81d3378da
Robert Sęk <robosek@tlen.pl>
2019-04-27 09:17:17 +0200

    Fix publish bash script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-ADDRESS=192.168.1.1
+ADDRESS=192.168.1.103
 
-fake.cmd run publish.fsx && echo - Entering publish folder... && cd sample/bin/Release/netcoreapp2.1/linux-arm/publish  && echo - Trying to send to files to raspberry pi && scp *.* pi@"$ADDRESS":project/ && echo "Done"
+bash fake.sh run publish.fsx && echo - Trying to send files to raspberry pi && scp -r sample/bin/Release/netcoreapp2.1/linux-arm/publish pi@"$ADDRESS":Desktop/ && echo Success
