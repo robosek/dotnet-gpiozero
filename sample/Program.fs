27205efbdcded887be113568ac9032ef4bfb3077
Robert Sęk <robosek@tlen.pl>
2019-04-27 16:16:49 +0200

    Add comments

--- sample/Program.fs
+++ sample/Program.fs
@@ -6,68 +6,60 @@ open DotnetGpiozero.Led
 [<EntryPoint>]
 let main argv =
 
+    //Initialize controllers 
     let robotController = RobotController()
     let ledController = LedController()
 
+    //Setup devices pins
     let leftMotorPins = NotCheckedPin.Create 27, NotCheckedPwmPin.Create 22
     let rightMotorPins = NotCheckedPin.Create 26, NotCheckedPwmPin.Create 17
     let ledPin = NotCheckedPin.Create 16
 
+    //Initialize pins
     let resultLedPin = tryOpenPin ledPin
     let resultLeftMotor = robotController.TryOpenMotorPins leftMotorPins
     let resultRightMotor = robotController.TryOpenMotorPins rightMotorPins
     
+    //Check pins initialization results
     let led = match resultLedPin with
               | Ok openedPin -> Led(openedPin)
               | Error e -> failwith(e)
-    
-    let ledEventHandler = led |> ledController.Blink 200
-    ledEventHandler.BlinkTask |> Async.RunSynchronously
 
     let robot = match resultLeftMotor, resultRightMotor with
                 | Ok motorLeft, Ok motorRight -> Robot(motorLeft, motorRight)
                 | Error error1, Error error2 -> failwith(error1 + ", " + error2)
                 | _, Error e -> failwith(e)
                 | Error e, _ -> failwith(e)
-
-
-
-    let forward = 
-        robotController.Forward   
     
-    let backward = 
-        robotController.Backward
-
-    let left = 
-        robotController.LeftSoft
-    
-    let right = 
-        robotController.RightSoft
-
-    let stop = 
-        robotController.Stop
-    
+    //Led blinking
+    let ledEventHandler = led |> ledController.Blink 200
+    ledEventHandler.BlinkTask |> Async.RunSynchronously
+    //Simulate waiting for connection or some other awaiting...
     Async.Sleep 2000 |> Async.RunSynchronously
+    //Awaiting done!
     ledEventHandler.StopBlinking()
 
+    Async.Sleep 1000 |> Async.RunSynchronously
+
+    //Let's the game begin
     led |> ledController.On
-    robot |> forward 1.0
+    robot |> robotController.Forward 1.0
     Console.WriteLine("Forward")
     Async.Sleep 2000 |> Async.RunSynchronously
 
-    robot |> backward 1.0
+    robot |> robotController.Backward 1.0
     Console.WriteLine("Backward")
     Async.Sleep 2000 |> Async.RunSynchronously
 
-    robot |> left 1.0
+    robot |> robotController.LeftSoft 1.0
     Console.WriteLine("Left")
     Async.Sleep 2000 |> Async.RunSynchronously
 
-    robot |> right 1.0
+    robot |> robotController.RightSoft 1.0
     Console.WriteLine("Right")
     Async.Sleep 2000 |> Async.RunSynchronously
 
-    robot |> stop
+    robot |> robotController.Stop
     Console.WriteLine("Stop")
     led |> ledController.Off
 
