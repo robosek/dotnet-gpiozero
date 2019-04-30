5b45d74162ecf893b09d824d121e5b8f31f12860
Robert Sęk <robosek@tlen.pl>
2019-04-30 16:24:42 +0200

    SoftPwm and HardwarePwm

--- src/shared/Types.fs
+++ src/shared/Types.fs
@@ -2,21 +2,24 @@ namespace DotnetGpiozero.Shared
 open System.Device.Gpio
 open System.Device.Pwm
 open System.Device.Pwm.Drivers
+open System.Timers
 
 type Pin = int
 type Channel = int
-type PwmPin = Pin * Channel
+type PwmPin = | SoftPwmPin of Pin | HardwarePwmPin of Pin * Channel
 
 type NotCheckedPin = private NotCheckedPin of Pin * GpioController
                      with static member Create pin = 
                             NotCheckedPin(pin, new GpioController())
 
 type NotCheckedPwmPin = private NotCheckedPwmPin of PwmPin * PwmController
-                        with static member Create pin = 
-                              NotCheckedPwmPin(pin, new PwmController(new UnixPwmDriver())) 
-                                
+                        with static member Create pin =
+                                match pin with
+                                | HardwarePwmPin _ -> NotCheckedPwmPin(pin, new PwmController(new UnixPwmDriver()))
+                                | SoftPwmPin _ -> NotCheckedPwmPin(pin, new PwmController(new SoftPwm()))
+
 type OpenedPin = private OpenedPin of Pin * GpioController
-                  with static member Create pin gpioController = 
+                 with static member Create pin gpioController = 
                                OpenedPin(pin, gpioController)
 
 type OpenedPwmPin = private OpenedPwmPin of PwmPin * PwmController
@@ -26,6 +29,6 @@ type OpenedPwmPin = private OpenedPwmPin of PwmPin * PwmController
 type EventStream = 
         {
             Task: Async<unit>; 
-            Observable: IEvent<System.Timers.ElapsedEventHandler,System.Timers.ElapsedEventArgs>
+            Observable: IEvent<ElapsedEventHandler, ElapsedEventArgs>
             StopTask: Unit -> Unit
         }
