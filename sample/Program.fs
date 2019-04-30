3b0735b59c2ad813ea3951a907bfe6136e9a7ad0
Robert Sęk <robosek@tlen.pl>
2019-04-30 16:25:50 +0200

    Add buzzer example, adjust soft/hardware pwm

--- sample/Program.fs
+++ sample/Program.fs
@@ -2,6 +2,7 @@
 open DotnetGpiozero.Robot
 open DotnetGpiozero.Shared
 open DotnetGpiozero.Led
+open DotnetGpiozero.Buzzer
 
 [<EntryPoint>]
 let main argv =
@@ -9,37 +10,51 @@ let main argv =
     //Initialize controllers 
     let robotController = RobotController()
     let ledController = LedController()
+    let buzzerController = BuzzerController()
 
     //Setup device pins
-    let leftMotorPins = NotCheckedPin.Create 27,NotCheckedPin.Create 22, NotCheckedPwmPin.Create (0, 0)
-    let rightMotorPins = NotCheckedPin.Create 26, NotCheckedPin.Create 17, NotCheckedPwmPin.Create (0, 1)
+    let leftMotorPins = NotCheckedPin.Create 27,NotCheckedPin.Create 22, NotCheckedPwmPin.Create (HardwarePwmPin(0,0))
+    let rightMotorPins = NotCheckedPin.Create 26, NotCheckedPin.Create 17, NotCheckedPwmPin.Create (HardwarePwmPin(0, 1))
     let ledPin = NotCheckedPin.Create 16
+    let buzzerPin = NotCheckedPwmPin.Create (SoftPwmPin(21))
 
     //Initialize pins
     let resultLedPin = tryOpenPin ledPin
     let resultLeftMotor = robotController.TryOpenMotorPins leftMotorPins
     let resultRightMotor = robotController.TryOpenMotorPins rightMotorPins
+    let resultBuzzerPin = tryOpenPwmPin buzzerPin
     
     //Check pins initialization results
     let led = match resultLedPin with
               | Ok openedPin -> Led(openedPin)
               | Error e -> failwith(e)
+    
+    let buzzer = match resultBuzzerPin with
+                  | Ok openedPin -> Buzzer(openedPin)
+                  | Error e -> failwith(e)
 
     let robot = match resultLeftMotor, resultRightMotor with
                 | Ok motorLeft, Ok motorRight -> Robot(motorLeft, motorRight)
                 | Error error1, Error error2 -> failwith(error1 + ", " + error2)
                 | _, Error e -> failwith(e)
                 | Error e, _ -> failwith(e)
+
+    //Beep
+    buzzer |> buzzerController.On 0.6
+    // buzzerEventHandler.BeepTask |> Async.RunSynchronously
+    Async.Sleep 2000 |> Async.RunSynchronously
+    // buzzerEventHandler.StopBeeping()
+    buzzer |> buzzerController.Off |> ignore
     
     //Led blinking
-    //let ledEventHandler = led |> ledController.Blink 200
-    //ledEventHandler.BlinkTask |> Async.RunSynchronously
+    let ledEventHandler = led |> ledController.Blink 200
+    ledEventHandler.BlinkTask |> Async.RunSynchronously
     //Simulate waiting for connection or some other awaiting...
-    //Async.Sleep 2000 |> Async.RunSynchronously
+    Async.Sleep 2000 |> Async.RunSynchronously
     //Awaiting done!
-    //ledEventHandler.StopBlinking()
+    ledEventHandler.StopBlinking()
 
-    //Async.Sleep 1000 |> Async.RunSynchronously
+    Async.Sleep 1000 |> Async.RunSynchronously
 
     //Let the game begin
     led |> ledController.On
