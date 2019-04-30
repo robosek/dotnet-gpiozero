6f751a2c068885e05b5a6f551beb64afbf816fda
Robert Sęk <robosek@tlen.pl>
2019-04-30 16:25:36 +0200

    Use softpwm instead of gpio pin

--- src/output-devices/buzzer/Buzzer.fs
+++ src/output-devices/buzzer/Buzzer.fs
@@ -1,23 +1,23 @@
 ﻿namespace DotnetGpiozero.Buzzer
 open DotnetGpiozero.Shared
-open System.Device.Gpio
 open System.Threading
 
 type BuzzerController() =
-    let beepOnce (pauseTime: int) (buzzer: Buzzer) =
-        buzzer.OpenedPin |> write PinValue.High
+    let beepOnce (pauseTime: int) volume (buzzer: Buzzer) =
+        buzzer.OpenedPwmPin |> writePwm volume
         Thread.Sleep pauseTime
-        buzzer.OpenedPin |> write PinValue.Low
+        buzzer.OpenedPwmPin |> writePwm volume
         Thread.Sleep pauseTime
 
-    member __.On (buzzer: Buzzer) = 
-        buzzer.OpenedPin |> write PinValue.High
+    member __.On volume (buzzer: Buzzer) = 
+        buzzer.OpenedPwmPin |> writePwm volume
    
     member __.Off (buzzer: Buzzer) = 
-        buzzer.OpenedPin |> write PinValue.Low
+        buzzer.OpenedPwmPin |> stopWritingPwm
+        buzzer.OpenedPwmPin |> tryClosePwmPin
     
-    member __.Beep (beepTime: int) (buzzer: Buzzer) =
+    member __.Beep (beepTime: int) volume (buzzer: Buzzer) =
         let eventStream = createTimerAndObservable beepTime
         eventStream.Observable 
-        |> Observable.subscribe (fun _ -> beepOnce beepTime buzzer) |> ignore
-        {BlinkTask = eventStream.Task; StopBlinking = eventStream.StopTask}
+        |> Observable.subscribe (fun _ -> beepOnce beepTime volume buzzer) |> ignore
+        {BeepTask = eventStream.Task; StopBeeping = eventStream.StopTask}
